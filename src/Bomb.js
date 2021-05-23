17725a7b75a1005b7b530788760079f250ebe32a
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2021-05-23 00:43:32 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,26 @@
 // your Bomb code here!
+
+import React from 'react'
+
+class Bomb extends React.Component {
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    countTracker = (seconds) => {
+        return seconds > 0 ? `${this.state.secondsLeft} seconds left before I go boom!` : "Boom!"
+    }
+
+    
+
+    render() {
+        return (
+            <div>{this.countTracker(this.state.secondsLeft)}</div>
+        )
+    }
+}
+
+export default Bomb
